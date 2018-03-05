4e13c92959dafa190c513f193904f09f7203b0e2
Miroslav P <miroslav.petkevic@gmail.com>
2018-03-05 12:39:22 +0200

    Commit (correct)

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $handle = fopen("locations.csv","r");
 
 while (($data = fgetcsv($handle)) !== false) {
     $jsonData = json_encode($data);
-    $json = file_put_contents("locations.json",$jsonData, FILE_APPEND);
+    file_put_contents("locations.json",$jsonData, FILE_APPEND);
 }
 
 echo "Done";
